fdb4bab500708c7019c01ef7ad830666b6eea35e
armulchandani <armulchandani@gmail.com>
2016-12-08 14:51:08 +0530

    Adding bare bones application configuration to the project

---
+++ src/main/java/com/opticores/config/ApplicationConfiguration.java
@@ -0,0 +1,24 @@
+package com.opticores.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+/** The basic class acting as a main configuration class looked upon by 
+ *  the spring framework and have following responsibilities:
+ *  
+ *   1. Enable Web MVC
+ *   2. Add automatic component scan specifying the base package
+ * 
+ * @author anubhav
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.opticores.controllers")
+@Import(DatabaseConfiguration.class)
+public class ApplicationConfiguration {
+
+}
