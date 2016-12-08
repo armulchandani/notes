3fd544ad6062ff40eb6bb8f9899fcc9ce7804c23
armulchandani <armulchandani@gmail.com>
2016-12-08 12:53:05 +0530

    Added a java class file holding application related constants

---
+++ src/main/java/com/opticores/common/ApplicationConstants.java
@@ -0,0 +1,28 @@
+package com.opticores.common;
+
+import com.opticores.config.DatabaseConfiguration;
+
+/** A simple class holding all the application related constants
+ *  avoiding hard coding in other application components.
+ *  
+ *  Referred from {@link DatabaseConfiguration}
+ * 
+ * 
+ * @author anubhav
+ *
+ */
+public class ApplicationConstants {
+	
+	public static final String PROPERTY_DATABASECONFIG="databaseconfig.properties";
+	
+	public static final String PROPERTY_HIBERNATE_DAILECT="hibernate.dialect";
+	
+	public static final String PROPERTY_JDBC_CONNECTION_URL="jdbc.url";
+	
+	public static final String PROPERTY_JDBC_USERNAME="jdbc.username";
+	
+	public static final String PROPERTY_JDBC_PASSWORD="jdbc.password";
+	
+	public static final String PROPERTY_JDBC_DRIVER="jdbc.driverClassName";
+
+}
