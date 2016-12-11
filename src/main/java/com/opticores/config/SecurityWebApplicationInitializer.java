27cec21f74a27a576aca662e75554bac1719ee11
armulchandani <armulchandani@gmail.com>
2016-12-11 20:29:42 +0530

    Added comments for better use of the class

--- src/main/java/com/opticores/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/opticores/config/SecurityWebApplicationInitializer.java
@@ -1,13 +1,23 @@
 package com.opticores.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 /**
+ * This is an important class, looked upon by Spring framework to create and
+ * register an instance of {@link DelegatingFilterProxy} from method
+ * {@link #onStartup(javax.servlet.ServletContext)}
+ * 
+ * 
+ * {@link DelegatingFilterProxy} and other filters in chain intercepts all the
+ * requests URIS as configured inside method {@link #configure(HttpSecurity)} of
+ * class {@link ApplicationSecurityConfiguration}
+ * 
+ * 
  * @author anubhav
  *
  */
 public class SecurityWebApplicationInitializer extends
 		AbstractSecurityWebApplicationInitializer {
-	
-	
+
 }
