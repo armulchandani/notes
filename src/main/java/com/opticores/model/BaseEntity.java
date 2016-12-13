5870eeaf0805b5b3c0390df7703eea9be45265be
armulchandani <armulchandani@gmail.com>
2016-12-13 09:14:24 +0530

    Added comments to this class for better readability

--- src/main/java/com/opticores/model/BaseEntity.java
+++ src/main/java/com/opticores/model/BaseEntity.java
@@ -5,16 +5,34 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+/**
+ * This is a base entity class having only one attribute 'id'. Extended by
+ * {@link Note} & {@link User}.
+ * 
+ * {link @MappedSuperClass} tells 'Hibernate' : hey hibernate the identifier for
+ * the actual entities could be found inside this BaseEntity following a design
+ * principle 'DRY'.
+ * 
+ * If {@link Note} & {@link User} don't extend from this class and no field
+ * annotated with {@link @Id} is found , hibernate runtime will throw a runtime error 'No
+ * identifier specified for entity'
+ * 
+ * 
+ * 
+ * @author anubhav
+ *
+ */
 @MappedSuperclass
 public class BaseEntity {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
